d8e850fdc33d90faaf3d0ed01cc68f17324102c2
芦明宝 <454747737@qq.com>
2025-03-21 15:29:53 +0800

    build: 更新 Docker 镜像版本号
    - 将 Docker镜像版本从 gsedu-v4.9.0 修改为 gsedu-v4.9.1- 此更新统一了项目的版本号，确保版本一致性

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=gsedu-v4.9.0
+VERSION=gsedu-v4.9.1
 
 echo '开始构建Docker镜像...'
 docker build -f ./projects/app/Dockerfile -t registry.cn-shanghai.aliyuncs.com/usoftgs/fastgpt:${VERSION} --platform linux/amd64 . --build-arg name=app
